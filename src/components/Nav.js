c42f80df9d0920750faf45dd64a6967f0d41a927
AbdullahAlHarun-code <>
2021-12-02 23:11:08 +0000

    add react router and page navigate

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,16 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/images/logo-bg.png";
 import classNames from "../styles/Nav.module.css";
 import Account from "./Account";
-
 export default function Nav() {
   return (
     <nav className={classNames.nav}>
       <ul>
         <li>
-          <a href="index.html" className={classNames.brand}>
+          <Link to="/" className={classNames.brand}>
             <img src={logo} alt="Learn with Sumit Logo" />
             <h3>Learn with Sumit</h3>
-          </a>
+          </Link>
         </li>
       </ul>
       <Account />
